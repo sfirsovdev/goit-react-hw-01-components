94318264759136bf96348fb562d8209dd72ddedc
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-06-27 16:51:52 +0300

    Финиш

---
+++ src/theme.js
@@ -0,0 +1,36 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    avatar: 'grey',
+    text: '#2a2a2a',
+    accent: 'gray',
+    accentText: '#8ca8b8',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+
+  lineHeights: {
+    body: 1.5,
+    heading: 1.2,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid gray',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
